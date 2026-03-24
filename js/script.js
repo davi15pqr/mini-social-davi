0e7609f26d9eed804abf0d75a53c7b7c90dccfc9
davi15pqr <davi.9552914@aluno.mg.gov.br>
2026-03-24 18:27:00 +0000

    alteraçoes foto e botão de curtir

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 let likeCount = 0;
+let curtido = false; // flag booleana
 
-function curtir() {
+  function curtir() {
+
+    if(curtido == false){
   likeCount++;
+  curtido = true;
+  document.getElementById("likeCount").innerText = likeCount;
+}else{
+  likeCount--;
+  curtido = false;
   document.getElementById("likeCount").innerText = likeCount;
 }
 
+}
+
 document.getElementById("likeBtn").addEventListener("click", curtir);
+
+   function descurtir() {
+
+    if(descurtido == false){
+  deslikeCount++;
+  descurtido = true;
+  document.getElementById("likeCount").innerText = likeCount;
+}else{
+  deslikeCount--;
+  descurtido = false;
+  document.getElementById("likeCount").innerText = likeCount;
+}
+
+}
+
+document.getElementById("likeBtn").addEventListener("click", curtir);
+
